82a6431a0aa6de4dc22625036ff4ec15b4ad6600
Hasan Aktas <developer@hasanaktas.com>
2024-07-16 14:34:50 +0300

    chore: Remove unused files and update assetlinks.json

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const userAgent = req.headers.get("user-agent") || "";
+
+  const isAndroid = /Android/i.test(userAgent);
+  const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
+
+  if (isAndroid) {
+    return NextResponse.redirect(
+      `https://play.google.com/store/apps/details?id=com.hipicon`
+    );
+  }
+
+  if (isIOS) {
+    return NextResponse.redirect(
+      `https://apps.apple.com/tr/app/hipicon-better-by-design/id1523203666`
+    );
+  }
+
+  return NextResponse.redirect(
+    `https://www.hipicon.com${req.nextUrl.pathname}${req.nextUrl.search}`
+  );
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
